87fd32b5f872a31a105b08e408492ca25851f7c3
Khadeeja Minha <khadeejaminha2002@gmail.com>
2025-07-15 11:44:52 +0530

    Created search and delete component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Adduser from './Components/Adduser';
+import Searchuser from './Components/Searchuser';
+import Deleteuser from './Components/Deleteuser';
 
 function App() {
   return (
     <div>
-
+      <Adduser/>
+      <Searchuser/>
+      <Deleteuser/>
     </div>
   );
 }
